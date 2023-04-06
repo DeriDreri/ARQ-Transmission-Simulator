d03ba8eb1472f751d1b087fbccea65323598eb22
Deri <dominikcybulski@protonmail.com>
2023-04-06 14:25:28 +0200

    Implements parity coding and sending to channel function

--- transmitter.py
+++ transmitter.py
@@ -1,28 +1,71 @@
 from bitstring import ConstBitStream
+from bitstring import ReadError
 
 class Transmitter:
 
-    def __init__(self, packetLength, extraBits):
+    #Konstruktor definiuje długość pakietów i nadmiarowość
+    def __init__(self, packetLength):
         self.packetLength = packetLength
-        self.extraBits = extraBits
+        self.tabOfBits = []
 
+    #Łaczy z plikiem odczytywanym
     def connectToInputFile(self, filePath):
         self.filePath = filePath
+        self.reachedEndOfFile = False
         self.bitsStream = ConstBitStream(filename = filePath)
     
 
+    #Podłącza do kanału
     def connectToChannel(self, channel):
         self.transmissionChannel = channel
 
+    #Testowa funkcja do drukowania pakietów bitów
     def printBites(self):
-        tab = []
-        for i in range (0, 7):
-            value = self.bitsStream.read(1).uint
-            tab.insert(i, value)
-        print(tab)
+        while(self.reachedEndOfFile == False):
+            self.loadBites()
+            if not self.tabOfBits:
+                 return
+            self.codePacket()
+            print(self.tabOfBits)      
+
+    #Ładuje nową porcję bitów do tablicy
+    def loadBites(self):
+        self.tabOfBits = []  #Czyści tablicę z poprzednich bitów
+        for i in range (0, self.packetLength):  
+            try:
+                value = self.bitsStream.read(1).uint
+                self.tabOfBits.insert(i, value)
+            except ReadError:  #Spodziewa się końca pliku
+                self.reachedEndOfFile = True    
+                return   
+
+
+    #Funkcja wysyłająca bity
+    def send(self, correctTransmission):
+        if(correctTransmission):
+            self.loadBites()
+            self.codePacket()
+
+        self.transmissionChannel.recieve(self.bitsStream)
+
+
+    def beginTransmission(self):
+        self.send(True)
+
+    #Funkcja wzywana wewnętrznie aby zakodować bit parzystości
+    def codePacket(self):
+        EvenOnes = True
+        for i in range(0, len(self.tabOfBits)):
+            if(self.tabOfBits[i] == 1):
+                EvenOnes = not EvenOnes
+
+        if (EvenOnes):
+            self.tabOfBits.insert(len(self.tabOfBits), 0)
+        else:
+            self.tabOfBits.insert(len(self.tabOfBits), 1)        
 
 def main():
-    transmitter = Transmitter(10, 0)
+    transmitter = Transmitter(4)
     transmitter.connectToInputFile('input-files/input.txt')
     transmitter.printBites()
 
