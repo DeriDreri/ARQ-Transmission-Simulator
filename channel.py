ae377ec9d9862de4a581efef10e1dad69c1b10dd
Jakub Jakubowicz <kubajak@outlook.com>
2023-04-08 18:35:53 +0200

    Generator liczb pseudolosowych

--- channel.py
+++ channel.py
@@ -1,5 +1,11 @@
 import bitstring
 
+# Prawdopodobieństwo wystąpienia błędu
+error_Rate = 0, 5
+
+# Ziarno generatora liczb pseudolosowych
+seed = 42
+
 
 class Channel:
 
@@ -7,3 +13,12 @@ class Channel:
     def __init__(self, transmitter, receiver):
         self.transmitter = transmitter
         self.receiver = receiver
+
+    # Generator liczb pseudolosowych
+    def random(seed, a=1103515245, c=12345, m=2 ** 31 - 1):
+        while True:
+            seed = (a * seed + c) % m
+            yield seed
+
+    generator = random(seed)
+
